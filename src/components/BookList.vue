8d18a69e75715c119bc36c6d59ddfa7c9b4c89f4
Zachary Johnson <zjohnson@frontlineed.com>
2019-02-14 19:13:13 -0600

    1
    1

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,12 +4,21 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
+    <hr>
+    <h2>Filtered Books by Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{filter}}</option>
+    </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,19 +29,26 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson",finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman",finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" ,finishedReading: true}
-      ]
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson",finishedReading: true, ownership: "borrowed" },
+        { title: "American Gods", author: "Neil Gaiman",finishedReading: false, ownership: "bought" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" ,finishedReading: true, ownership: "borrowed"}
+      ],
+      filters: ["bought","borrowed"],
+      holding: "bought"
     };
   },
   components: {
     BookItem,
     BookForm
   },
+  computed:{
+    filteredBooks(){
+      return _.filter(this.books,["ownership", this.holding]);
+    }
+  },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor,finishedReading: bookData.finishedReading });
+      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor,finishedReading: bookData.finishedReading, ownership: bookData.ownership });
     }
   }
 };
